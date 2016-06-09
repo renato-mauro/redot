5dbd88153b88591812b898a647d42a22883e337b
Renato Mauro <renato.mauro@gmail.com>
2016-06-09 17:33:19 -0300

    Website adjustments

--- examples/filter/filter.js
+++ examples/filter/filter.js
@@ -1,19 +1,19 @@
 var global = { selectedItem: null };
 
 var data = [];
-for(var i=0; i<20; i++)
+for(var i=0; i<50; i++)
 {
     data.push({qtd:~~(Math.random()*50)+10});
 }
 var d1 = redot.filter(data,"{d.qtd<30}");
 var d2 = d1.notSelected();
 
-// setTimeout(function(){data.sort(function(a,b){return a.qtd-b.qtd});redot.changed(data)},5000);
+setTimeout(function(){data.sort(function(a,b){return a.qtd-b.qtd});redot.changed(data)},5000);
 
 function GraficoDeBarras()
 {    
-    this.width    = 500;
-    this.height   = 70;
+    this.width    = 1024;
+    this.height   = 150;
     this.barWidth = "{width/d.length}";
 
     /*******************************************************
