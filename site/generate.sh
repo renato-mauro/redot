971926a6a7818a37f8e9af469628dbaf1825b46b
Renato Mauro <renato.mauro@gmail.com>
2016-06-09 16:32:18 -0300

    Website adjustments

--- site/generate.sh
+++ site/generate.sh
@@ -56,7 +56,7 @@ function indexPage {
 	echo '
 		    </div>
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-			<script src="../js/bootstrap.min.js"></script>		    
+			<script src="site/js/bootstrap.min.js"></script>		    
 		  </body>
 		</html>
 	' >> ../index.html	
